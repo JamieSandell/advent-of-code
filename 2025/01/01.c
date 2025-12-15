a5d10feeaaaa09f20c3b3fb04039a1241c4b9656
Jamie Sandell <jamie.sandell@outlook.com>
2025-12-15 08:08:58 +0000

    2025-12-01

--- 2025/01/01.c
+++ 2025/01/01.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *combinations[] =
-{
-    "S50",
-    "L68",
-    "L30",
-    "R48",
-    "L5",
-    "R60",
-    "L55",
-    "L1",
-    "L99",
-    "R14",
-    "L82"
-};
+// char *combinations[] =
+// {
+//     "L68",
+//     "L30",
+//     "R48",
+//     "L5",
+//     "R60",
+//     "L55",
+//     "L1",
+//     "L99",
+//     "R14",
+//     "L82"
+// };
 
 typedef struct Node
 {
@@ -26,13 +25,97 @@ typedef struct Node
 Node *head = NULL;
 Node *tail = NULL;
 
+typedef struct Input
+{
+    FILE *file;
+    char *buffer;
+    long number_of_bytes;
+} Input;
+
 void insert_at_end(int value);
 
 void make_list_circular(void);
 
-void process_combinations(void);
+struct Input *read_input(void);
+
+void process_combinations(const char *combinations);
 
 struct Node *create_node(int value);
+
+void print_list(void);
+
+void null_terminate_strings(char *buffer);
+
+int main(void)
+{
+    for(int i = 0; i < 100; ++i)
+    {
+        insert_at_end(i);
+    }
+
+    make_list_circular();
+
+    print_list();
+
+    Input* input = read_input();
+
+    null_terminate_strings(input->buffer);
+
+    process_combinations(input->buffer);
+
+    return EXIT_SUCCESS;
+}
+
+// assumes the input is a null terminated string, converts newlines into null terminated strings
+void null_terminate_strings(char *buffer)
+{
+    for(; *buffer != '\0'; ++buffer)
+    {
+        if (*buffer == '\n')
+        {
+            *buffer = '\0';
+        }
+    }
+}
+
+// assumes input.txt is present
+struct Input *read_input(void)
+{
+    Input *input = malloc(sizeof(Input));
+
+    if(input == NULL)
+    {
+        printf("Error: Failed to create memory for input.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input->file = fopen("input.txt", "r");
+
+    if(input->file == NULL)
+    {
+        printf("Error: Failed to open input.txt for reading.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(input->file, 0L, SEEK_END);
+    input->number_of_bytes = ftell(input->file) + 1; // so we can null terminate it
+    fseek(input->file, 0L, SEEK_SET);
+
+    input->buffer = calloc(input->number_of_bytes, sizeof(char));
+
+    if (input->buffer == NULL)
+    {
+        printf("Error: Failed to allocate memory for input->buffer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fread(input->buffer, sizeof(char), input->number_of_bytes, input->file);
+    fclose(input->file);
+
+    printf("contents of input.txt\n\n%s", input->buffer);
+
+    return input;
+}
 
 struct Node *create_node(int value)
 {
@@ -141,22 +224,16 @@ Node *set_dial_to_starting_position(Node *current, int destination)
     return current;
 }
 
-// assumes at least 2 combinations present, the first is the starting point, the second onwards are the direction to turn and the amount to turn by
-void process_combinations(void)
+// assumes 50 is the starting point on the dial and all input is valid (LX, RX, e.g. L15, R1)
+void process_combinations(const char *combinations)
 {
-    int number_of_combinations = sizeof(combinations) / sizeof(combinations[0]);
-
-    char *starting_point_digits = combinations[0] + 1;
-    int starting_point = convert_string_to_int(starting_point_digits);
-    printf("Starting point is: %d\n", starting_point);
-
     Node *current = head;
-    current = set_dial_to_starting_position(current, starting_point);
+    current = set_dial_to_starting_position(current, 50);
     printf("Dial has been turned to %d\n", current->data);
 
     int password = 0;
 
-    for (int i = 1; i < number_of_combinations; ++i)
+    for (int i = 0; i < number_of_combinations; ++i)
     {
         char direction = *(combinations[i]);
         char *digits = combinations[i] + 1;
@@ -182,20 +259,4 @@ void process_combinations(void)
     }
 
     printf("The password is: %d\n", password);
-}
-
-int main(void)
-{
-    for(int i = 0; i < 100; ++i)
-    {
-        insert_at_end(i);
-    }
-
-    make_list_circular();
-
-    print_list();
-
-    process_combinations();
-
-    return EXIT_SUCCESS;
 }
