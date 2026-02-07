b7984c8f53d5aa9626ed4fd01691320b2e810f17
Jamie Sandell <jamie.sandell@outlook.com>
2026-02-07 22:49:49 +0000

    2025/12/03

--- 2025/03/03.c
+++ 2025/03/03.c
@@ -5,14 +5,40 @@
 
 int main(void)
 {
-    char *input = read_entire_file_into_buffer();
-    
+    char *input = read_entire_file_into_buffer();    
     char *token = strtok(input, "\n");
+    int maximum_joltage = 0; 
 
     while (token != NULL)
     {
+        int largest_number = 0;
+        int second_largest_number = 0;
+        char *c = token;        
+
+        while (*c != '\0')
+        {
+            int battery = (*c) - '0';
+            
+            if (battery >= largest_number)
+            {
+                largest_number = battery;
+            }
+            else if(battery >= second_largest_number)
+            {
+                if ((largest_number * 10) + battery > (largest_number * 10) + second_largest_number)
+                {
+                    second_largest_number = battery;
+                }
+            }
+
+            ++c;
+        }
+
+        maximum_joltage += ((largest_number * 10) + second_largest_number);
         token = strtok(NULL, "\n");
     }
+
+    fprintf(stdout, "\nMaximum joltage is: %d\n", maximum_joltage);
 
     return EXIT_SUCCESS;
 }
